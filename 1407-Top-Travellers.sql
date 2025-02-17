b427abb90e69376f57a8b4da07f726d45d5ece62
Hagar Galal <hagargalalstu@gmail.com>
2025-02-18 01:46:10 +0200

    Delete 1407-Top-Travellers.sql

--- 1407-Top-Travellers.sql
+++
@@ -1,5 +0,0 @@
--- Write your PostgreSQL query statement below
-SELECT Users.name, COALESCE(SUM(distance), 0) AS travelled_distance FROM Users
-LEFT JOIN Rides ON Users.id = Rides.user_id
-GROUP BY Rides.user_id, Users.name
-ORDER BY travelled_distance DESC, name ASC;  
